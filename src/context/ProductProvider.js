ad38462c4c5a9d0104f2a10a3c3c361bab343eef
MohammadFahad1 <fahad4bangladesh@gmail.com>
2023-02-02 01:08:30 +0600

    Home Page Products Successfully done with full reducer & context feature

--- src/context/ProductProvider.js
+++ src/context/ProductProvider.js
@@ -1,19 +1,27 @@
 import React, { useContext } from 'react';
+import { useReducer } from 'react';
 import { useEffect } from 'react';
-import { useState } from 'react';
 import { createContext } from 'react';
+import { actionType } from '../state/ProductState/actionTypes';
+import { initialState, productReducer } from '../state/ProductState/ProductReducer';
 
 const PRODUCT_CONTEXT = createContext();
 
 const ProductProvider = ({ children }) => {
-    const [data, setData] = useState([]);
+
+    const [state, dispatch] = useReducer(productReducer, initialState);
+
     useEffect(() => {
+        dispatch({ type: actionType.FETCHING_START });
         fetch('products.json')
             .then(res => res.json())
-            .then(data => setData(data))
+            .then(data => dispatch({ type: actionType.FETCHING_SUCCESS, payload: data }))
+            .catch(() => {
+                dispatch({ type: actionType.FETCHING_ERROR })
+            })
     }, [])
 
-    const value = { data, setData };
+    const value = { state, dispatch };
     return <PRODUCT_CONTEXT.Provider value={value}>
         {children}
     </PRODUCT_CONTEXT.Provider>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,29 @@ import ProductCard from "../components/ProductCard";
 import { useProducts } from "../context/ProductProvider";
 
 const Home = () => {
-  const { data } = useProducts();
+  const { state: { products, loading, error } } = useProducts();
+
+  let content;
+
+  if (loading) {
+    content = <p>Loading</p>
+  }
+
+  if (error) {
+    content = <p>Sorry, Something went wrong!</p>
+  }
+
+  if (!loading && !error && products.length === 0) {
+    content = <p>No Products Found!</p>
+  }
+
+  if (!loading && !error && products.length) {
+    content = products.map(product => <ProductCard key={product.id} product={product} />)
+  }
+
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      {
-        data.map(product => <ProductCard product={product} />)
-      }
+      {content}
     </div>
   );
 };
