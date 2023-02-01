e2dedb1516aa61bfcea5173e9cd2aa9cda64a7d1
MohammadFahad1 <fahad4bangladesh@gmail.com>
2023-02-02 01:25:49 +0600

    Add To Cart Feature Added without testing whether working or not

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { BiListPlus } from "react-icons/bi";
+import { useProducts } from "../context/ProductProvider";
+import { actionType } from "../state/ProductState/actionTypes";
 
 const ProductCard = ({ product }) => {
-
+  const { dispatch } = useProducts();
   return (
     <div
       className='shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900'
@@ -19,7 +21,7 @@ const ProductCard = ({ product }) => {
         </ul>
       </div>
       <div className='flex gap-2 mt-5'>
-        <button
+        <button onClick={dispatch({ type: actionType.ADD_TO_CART, payload: { product } })}
           className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'>
           Add to cart
         </button>
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,11 +1,31 @@
 import React from "react";
 import ProductCard from "../components/ProductCard";
+import { useProducts } from "../context/ProductProvider";
 
 const Cart = () => {
+  const { state: { cart, loading, error } } = useProducts();
+
+  let content;
+
+  if (loading) {
+    content = <p>Loading</p>
+  }
+
+  if (error) {
+    content = <p>Sorry, Something went wrong!</p>
+  }
+
+  if (!loading && !error && cart.length === 0) {
+    content = <p>No Products Found!</p>
+  }
+
+  if (!loading && !error && cart.length) {
+    content = cart.filter(product => product.rating >= 4).map(product => <ProductCard key={product.id} product={product} />)
+  }
 
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      <ProductCard />
+      {content}
     </div>
   );
 };
